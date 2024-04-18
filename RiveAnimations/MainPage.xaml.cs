6c484e99663d0244f8b44896763a247070ef1c3a
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-04-18 15:48:52 -0500

    Added Rive Animation Demo

---
+++ RiveAnimations/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RiveAnimations
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
